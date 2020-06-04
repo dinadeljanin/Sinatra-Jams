02cf91ba0b9ea85b2f263dfdddb736a111504bce
“dinadeljanin” <ddeljanin5@icloud.com>
2020-06-04 16:46:58 -0400

    completed create and read deliverables

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,35 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
+    # should there be a form here to create a new one?
     erb :welcome
   end
 
+  get '/artists/new' do
+    erb :new
+  end
+
+  # post for new here
+  post '/artists' do
+    @artist = Artist.create(name: params[:name], age: params[:age], bio: params[:bio])
+    redirect "/artists/#{@artist.id}"
+  end
+
+  # show all
+  get '/artists' do
+    @artists = Artist.all
+    erb :index
+  end
+
+  # show one
+  get '/artists/:id' do
+    @artist = Artist.find(params[:id])
+    erb :show
+  end
+
+  # edit
+  get 'artists/:id/edit' do
+    @artist = Artist.find(params[:id])
+    erb :edit
+  end
 end
